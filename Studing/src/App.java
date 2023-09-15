c6f7281d5a1589f58170140fb6992f7704170e6e
italomoraess <italomoraes.sth@gmail.com>
2023-09-15 18:38:01 -0300

    Methods getters and setters

--- Studing/src/App.java
+++ Studing/src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Pessoa pessoa1 = new Pessoa("Italo Moraes", 26);
+        
+        Pessoa pessoa1 = new Pessoa("Italo Moraes", 26, "059.741.313-47");
+        pessoa1.setName("Italo Moraes Moura Gurgel");
 
-        System.out.println(pessoa1.retornaName());
-        System.out.println(pessoa1.retornaAge());
+        System.out.println(pessoa1.getName());
+        System.out.println(pessoa1.getAge());
+        System.out.println(pessoa1.getCpf());
     }
 }
--- Studing/src/Pessoa.java
+++ Studing/src/Pessoa.java
@@ -1,18 +1,34 @@
 public class Pessoa {
-  private String name;
-  private Integer age;
 
-  Pessoa(String nome, Integer age){
-    this.name = nome;
+  private String name;
+  private int age;
+  private String cpf;
+
+  Pessoa(String name, int age, String cpf){
+    this.name = name;
+    this.age = age;
+    this.cpf = cpf;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
     this.age = age;
   }
 
-  public String retornaName() {
-    return this.name;
+  public String getCpf() {
+    return cpf;
   }
 
-  public Integer retornaAge() {
-    return this.age;
-  }
 
 }
